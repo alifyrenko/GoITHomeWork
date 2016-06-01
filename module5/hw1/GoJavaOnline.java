9809c65f84c6714182f448672084c4156bd8b52c
Lifyrenko Anton <alifyrenko@gmail.com>
2016-06-01 10:15:35 +0300

    uodate

--- module5/hw1/GoJavaOnline.java
+++ module5/hw1/GoJavaOnline.java
@@ -1,5 +1,7 @@
 package com.goit.gojavaonline.module5.hw1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,43 +10,38 @@ import java.util.Scanner;
 
 public class GoJavaOnline {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        int minValue = 0;
+        int maxValue = 0;
 
         System.out.println("Please, enter number of values of your arrayList");
 
         int numOfValuesOfArrayList = scanner.nextInt();
-        int [] arrayList = new int[numOfValuesOfArrayList];
+        int[] arrayList = new int[numOfValuesOfArrayList];
 
         for (int i = 0; i < numOfValuesOfArrayList; i++) {
             System.out.println("Please, enter " + (i + 1) + " of " + numOfValuesOfArrayList + " value of your array");
             arrayList[i] = scanner.nextInt();
         }
 
-        boolean swapOccurred = true;
-
-        while (swapOccurred) {
-            swapOccurred = false;
-            for (int i = 0; i < arrayList.length - 1; i++) {
-                if (arrayList[i] > arrayList[i + 1]) {
-                    int tempValue = arrayList[i];
-                    arrayList[i] = arrayList[i + 1];
-                    arrayList[i + 1] = tempValue;
-
-                    swapOccurred = true;
-                }
+       /* for (int i = 0; i < arrayList.length; i++) {
+            if (i == 0) {
+                minValue = arrayList[0];
+                maxValue = arrayList[0];
             }
-        }
+            if (arrayList[i] < minValue) {
+                minValue = arrayList[i];
+            } else if (arrayList[i] > maxValue) {
+                maxValue = arrayList[i];
+            }
+        }*/
 
-        System.out.println("Below you can find your sorted array list:");
-        for (double arrayListPrint : arrayList) {
-            System.out.println(arrayListPrint);
-        }
+        Arrays.sort(arrayList);
+        minValue = arrayList[0];
+        maxValue = arrayList[arrayList.length-1];
 
-        int minValueOfArray = arrayList[0];
-        int maxValueOfArray = arrayList[arrayList.length - 1];
-
-        System.out.println("Minimum of the array is " + minValueOfArray);
-        System.out.println("Maximum of the array is " + maxValueOfArray);
-
+        System.out.println("Minimum of the array is " + minValue);
+        System.out.println("Maximum of the array is " + maxValue);
     }
 }
